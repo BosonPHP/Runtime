93a6fc492864f7516358a8276420c5c6daf1a553
Kirill Nesmeyanov <nesk@xakep.ru>
2025-06-20 17:49:28 +0300

    Fix ssse3 instruction set

--- libs/component/cpu-info/src/InstructionSet/Factory/CpuId/SSSE3Detector.php
+++ libs/component/cpu-info/src/InstructionSet/Factory/CpuId/SSSE3Detector.php
@@ -14,17 +14,17 @@ final readonly class SSSE3Detector extends AMD64Detector
     {
         $detector = $executor->compile(
             signature: 'int32_t(*)()',
-            code: "\xB8\x01\x00\x00\x00"      // mov    eax,0x1
+            code: "\xB8\x01\x00\x00\x00"      // mov eax,0x1
                 . "\x0F\xA2"                  // cpuid
-                . "\xF7\xC1\x00\x02\x00\x00"  // test   ecx,0x00000200 (1 << 9)
-                . "\x74\x05"                  // jz     no_ssse3
-                . "\xB0\x01"                  // mov    al,0x1
+                . "\xF7\xC1\x00\x02\x00\x00"  // test ecx, 0x00000200 (1 << 9)
+                . "\x74\x05"                  // jz no_ssse3
+                . "\xB0\x01"                  // mov al,0x1
                 . "\xC3"                      // ret
-                . "\x30\xC0"                  // xor    al,al
+                . "\x30\xC0"                  // xor al,al
                 . "\xC3"                      // ret
         );
 
         /** @phpstan-ignore-next-line : Known ignored issue */
-        return $detector() ? InstructionSet::SSE3 : null;
+        return $detector() ? InstructionSet::SSSE3 : null;
     }
 }
