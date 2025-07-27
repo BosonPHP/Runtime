5e6af99995b06f73ec73a76889e404864a546618
Kirill Nesmeyanov <nesk@xakep.ru>
2025-07-28 00:09:08 +0300

    Add URI contracts package

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -25,6 +25,7 @@ $files = PhpCsFixer\Finder::create()
         __DIR__ . '/libs/contracts/event-listener-contracts/src',
         __DIR__ . '/libs/contracts/http-contracts/src',
         __DIR__ . '/libs/contracts/id-contracts/src',
+        __DIR__ . '/libs/contracts/uri-contracts/src',
         __DIR__ . '/libs/contracts/value-object-contracts/src',
     ])
     ->filter(static fn (\SplFileInfo $file): bool
