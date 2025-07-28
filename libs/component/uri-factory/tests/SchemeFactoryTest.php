bed6167bdd1015f15fa3b691890234badff7cd61
Kirill Nesmeyanov <nesk@xakep.ru>
2025-07-28 16:00:05 +0300

    Add uri factory tests

---
+++ libs/component/uri-factory/tests/SchemeFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Component\Uri\Factory\Tests;
+
+use Boson\Component\Uri\Factory\SchemeFactory;
+use Boson\Component\Uri\Component\Scheme;
+use PHPUnit\Framework\Attributes\Before;
+use PHPUnit\Framework\Attributes\Group;
+
+#[Group('boson-php/uri-factory')]
+final class SchemeFactoryTest extends TestCase
+{
+    private SchemeFactory $factory;
+
+    #[Before]
+    protected function createPathFactory(): void
+    {
+        $this->factory = new SchemeFactory();
+    }
+
+    public function testCreateSchemeFromStringWithStandardScheme(): void
+    {
+        $scheme = $this->factory->createSchemeFromString('HTTP');
+
+        self::assertSame('http', $scheme->toString());
+    }
+
+    public function testCreateSchemeFromStringWithUserDefinedScheme(): void
+    {
+        $scheme = $this->factory->createSchemeFromString('custom-scheme');
+
+        self::assertInstanceOf(Scheme::class, $scheme);
+        self::assertSame('custom-scheme', $scheme->toString());
+    }
+}
